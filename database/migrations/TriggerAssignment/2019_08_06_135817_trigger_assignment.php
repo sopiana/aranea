e67f7bc21233f8305f4e2e96ccecfc20763771b2
yang sopiana <yang.sopiana@gmail.com>
2019-08-07 00:24:37 +0700

    Add update trigger to User Management Tables

--- database/migrations/TriggerAssignment/2019_08_06_135817_trigger_assignment.php
+++ database/migrations/TriggerAssignment/2019_08_06_135817_trigger_assignment.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
+use Symfony\Component\Console\Output\ConsoleOutput;
 class TriggerAssignment extends Migration
 {
     /**
@@ -34,6 +34,34 @@ class TriggerAssignment extends Migration
                 VALUES (NEW.updated_at, \'role_rights\', NEW.id, \'CREATE\', NEW.last_author, NULL, NULL, CONCAT(NEW.role_id,\' : \',NEW.right_id));'
             );
     }
+
+    private function buildOnUpdateTriggerCommand($table)
+    {
+        $columns = Schema::getColumnListing($table);
+        $triggerCmd = 'CREATE TRIGGER `'.$table.'_update_action` AFTER UPDATE ON `'.$table.'`
+            FOR EACH ROW
+                BEGIN ';
+        foreach($columns as $column)
+        {
+            if(($column!='last_author')&&($column!='updated_at')&&
+                ($column!='remember_token')&&($column!='id'))
+            {
+                $triggerCmd = $triggerCmd.'IF NOT(NEW.'.$column.' <=> OLD.'.$column.') THEN
+                            INSERT INTO `user_mngm_audits` (`effective_utc`, `source`, `source_id`, `type`, `author`, `column`, `old_value`, `new_value`)
+                            VALUES (NEW.updated_at, \''.$table.'\', NEW.id, \'UPDATE\', NEW.last_author, \''.$column.'\', OLD.'.$column.', NEW.'.$column.');
+                        END IF; ';
+            }
+        }
+        $triggerCmd = $triggerCmd.'END';
+        return $triggerCmd;
+    }
+    private function UserManagementTablesUpdateActions()
+    {
+        DB::unprepared($this->buildOnUpdateTriggerCommand('users'));
+        DB::unprepared($this->buildOnUpdateTriggerCommand('rights'));
+        DB::unprepared($this->buildOnUpdateTriggerCommand('roles'));
+        DB::unprepared($this->buildOnUpdateTriggerCommand('role_rights'));
+    }
     /**
      * Run the migrations.
      *
@@ -42,6 +70,7 @@ class TriggerAssignment extends Migration
     public function up()
     {
         $this->UserManagementTablesInsertActions();
+        $this->UserManagementTablesUpdateActions();
     }
 
     /**
